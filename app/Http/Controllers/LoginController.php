c7393ede016caa6f107cfd24ab258966f989b431
Paolo Scarentino <paolo.scarentino@gmail.com>
2025-07-29 16:30:25 +0200

    removed code for redirecting, now using middleware

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -16,10 +16,6 @@ class LoginController extends BaseController
 {
     public function showLoginForm()
     {
-        // Se l'utente è già autenticato, reindirizza alla dashboard
-        if (auth()->check()) {
-            return redirect()->route('dashboard');
-        }
         // Mostra il modulo di login
         return view('auth.login');
     }
